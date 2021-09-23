350cdacc8cb285293b933e8287603ed7381c2278
andrzejolszak <andrzejolszak@gmail.com>
2021-09-23 22:32:14 +0200

    Added ParallelHelper Nuget reference. Added PH007Example.

--- test/IntegrationTests/IntegrationTests.Cli/Program.cs
+++ test/IntegrationTests/IntegrationTests.Cli/Program.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Threading;
 
 namespace IntegrationTests.Cli {
   class Program {
     static void Main(string[] args) {
       Console.WriteLine("Hello World!");
+    }
+  }
+
+  class PH007Example {
+    public PH007Example() {
+      var thread = new Thread(() => { });
+
+      // PH_S007 warning expected below:
+      thread.Start();
     }
   }
 }

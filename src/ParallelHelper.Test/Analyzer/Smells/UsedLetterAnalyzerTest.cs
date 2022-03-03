c5bd4a30301fd096bb29645725eb07bca2b17c70
ebquhf <barna211@vipmail.hu>
2022-03-03 16:40:46 +0100

    creates UsedLetterAnalyzer and test to test functions

---
+++ src/ParallelHelper.Test/Analyzer/Smells/UsedLetterAnalyzerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelHelper.Analyzer.Smells;
+
+namespace ParallelHelper.Test.Analyzer.Smells {
+  [TestClass]
+  public class UsedLetterAnalyzerTest : AnalyzerTestBase<UsedLetterAnalyzer> {
+    [TestMethod]
+    public void ClassContainsLetterA() {
+      const string source = @"
+ public class Class1
+    {
+    }";
+      VerifyDiagnostic(source,new DiagnosticResultLocation(1,15));
+    }
+
+    [TestMethod]
+    public void ClassNotConatainsLetterA() {
+      const string source = @"
+ public class ThisIsIt
+    {
+    }";
+      VerifyDiagnostic(source);
+
+    }
+
+  }
+}

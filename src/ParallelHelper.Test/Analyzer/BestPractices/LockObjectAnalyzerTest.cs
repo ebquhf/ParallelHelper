a8599eda7d4d9e0451dd4820b17943bf98a0e8af
ebquhf <barna211@vipmail.hu>
2022-03-29 20:28:09 +0200

    Adds LockObjectAnalyzer skeleton

---
+++ src/ParallelHelper.Test/Analyzer/BestPractices/LockObjectAnalyzerTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelHelper.Analyzer.BestPractices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParallelHelper.Test.Analyzer.BestPractices {
+  [TestClass]
+  public class LockObjectAnalyzerTest : AnalyzerTestBase<LockObjectAnalyzer> {
+    [TestMethod]
+    public void ObjectLockedInClass() {
+      var source = @"public class Class
+      {
+            private readonly object lockObject = new object();
+            public int MyNumber;
+
+            public void DoWork()
+            {
+                lock (lockObject)
+                {
+                    MyNumber+=1;
+                }
+        }
+      }";
+      VerifyDiagnostic(source);
+    }
+  }
+}
---
+++ src/ParallelHelper/Analyzer/BestPractices/LockObjectAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace ParallelHelper.Analyzer.BestPractices {
+  [DiagnosticAnalyzer(LanguageNames.CSharp)]
+  public class LockObjectAnalyzer : DiagnosticAnalyzer {
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
+
+    public override void Initialize(AnalysisContext context) {
+      throw new NotImplementedException();
+    }
+  }
+}

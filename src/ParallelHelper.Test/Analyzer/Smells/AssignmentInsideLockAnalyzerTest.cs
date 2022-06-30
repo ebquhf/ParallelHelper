c7b269362c1015ba0f8baf9944ff972fa8626d4e
ebquhf <barna211@vipmail.hu>
2022-06-30 17:44:12 +0200

    Creates AssignmentInsideLockAnalyzer class and the corresponding test class

---
+++ src/ParallelHelper.Test/Analyzer/Smells/AssignmentInsideLockAnalyzerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelHelper.Analyzer.Smells;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelHelper.Test.Analyzer.Smells {
+  [TestClass]
+  public class AssignmentInsideLockAnalyzerTest : AnalyzerTestBase<AssignmentInsideLockAnalyzer> {
+    [TestMethod]
+    public void ObjectLockedInClass() {
+      var source = @"public class Class
+      {
+            private readonly object lockObject = new object();
+            
+            private int MyNumber;
+
+            public void DoWork()
+            {
+                lock (lockObject)
+                {
+                    MyNumber+=1;
+                }
+        }
+      }";
+      VerifyDiagnostic(source, new DiagnosticResultLocation(8, 17));
+    }
+  }
+}
---
+++ src/ParallelHelper/Analyzer/Smells/AssignmentInsideLockAnalyzer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace ParallelHelper.Analyzer.Smells {
+  [DiagnosticAnalyzer(LanguageNames.CSharp)]
+  public class AssignmentInsideLockAnalyzer : DiagnosticAnalyzer {
+    public const string DiagnosticId = "PH_BT002";
+    private const string Category = "Locking";
+
+    private static readonly LocalizableString Title = "Locking with object";
+    private static readonly LocalizableString MessageFormat = "Analyze if locking is used with an object";
+    private static readonly LocalizableString Description = "";
+
+    private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+     DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error,
+     isEnabledByDefault: true, description: Description, helpLinkUri: ""//gets the .md file from parallell helper github HelpLinkFactory.CreateUri(DiagnosticId)
+   );
+//    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
+
+    public override void Initialize(AnalysisContext context) {
+      throw new NotImplementedException();
+    }
+  }
+}

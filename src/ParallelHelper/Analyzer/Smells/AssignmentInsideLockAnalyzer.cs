d537e06f8e989ff3aae66ab57c7fcaa306ac233e
ebquhf <barna211@vipmail.hu>
2022-06-30 17:51:50 +0200

    Creates the logic for finding an assignment in a lock

--- src/ParallelHelper/Analyzer/Smells/AssignmentInsideLockAnalyzer.cs
+++ src/ParallelHelper/Analyzer/Smells/AssignmentInsideLockAnalyzer.cs
@@ -1,18 +1,21 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using System.Text;
 
 namespace ParallelHelper.Analyzer.Smells {
   [DiagnosticAnalyzer(LanguageNames.CSharp)]
   public class AssignmentInsideLockAnalyzer : DiagnosticAnalyzer {
-    public const string DiagnosticId = "PH_BT002";
+    public const string DiagnosticId = "PH_BT003";
     private const string Category = "Locking";
 
-    private static readonly LocalizableString Title = "Locking with object";
-    private static readonly LocalizableString MessageFormat = "Analyze if locking is used with an object";
+    private static readonly LocalizableString Title = "Assignment inside a lock";
+    private static readonly LocalizableString MessageFormat = "A variable has been assigned inside the lock";
     private static readonly LocalizableString Description = "";
 
     private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
@@ -21,10 +24,21 @@ namespace ParallelHelper.Analyzer.Smells {
    );
 //    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
     public override void Initialize(AnalysisContext context) {
-      throw new NotImplementedException();
+      context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+      context.EnableConcurrentExecution();
+      context.RegisterSyntaxNodeAction(AnalyzeLockStatement, SyntaxKind.LockStatement);
+    }
+    private void AnalyzeLockStatement(SyntaxNodeAnalysisContext ctx) {
+      var lockStatement = ctx.Node as LockStatementSyntax;
+      if(lockStatement != null && lockStatement.DescendantNodesAndSelf().OfType<AssignmentExpressionSyntax>().Any()) {
+        var location = lockStatement.GetLocation();
+        var diagnostic = Diagnostic.Create(Rule, location, "Assignment is used");
+
+        ctx.ReportDiagnostic(diagnostic);
+      }
     }
   }
 }

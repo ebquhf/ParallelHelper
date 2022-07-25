831bcf0438758aa7342a9f9c6a02177783bb363a
ebquhf <barna211@vipmail.hu>
2022-07-25 16:09:26 +0200

    creates MethodCallInsideLockAnalyzer class and its tests

---
+++ src/ParallelHelper.Test/Analyzer/Smells/MethodCallInsideLockAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelHelper.Test.Analyzer.Smells {
+  [DiagnosticAnalyzer(LanguageNames.CSharp)]
+  public class MethodCallInsideLockAnalyzer : DiagnosticAnalyzer {
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
+
+    public override void Initialize(AnalysisContext context) {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ src/ParallelHelper.Test/Analyzer/Smells/MethodCallInsideLockAnalyzerTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelHelper.Test.Analyzer.Smells {
+  [TestClass]
+  public class MethodCallInsideLockAnalyzerTest:AnalyzerTestBase<MethodCallInsideLockAnalyzer> {
+  }
+}

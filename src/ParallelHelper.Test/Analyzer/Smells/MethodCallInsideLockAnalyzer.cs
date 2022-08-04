755b0ee0d07231bced5524262b69129b1526e5e1
ebquhf <barna211@vipmail.hu>
2022-08-04 21:14:09 +0200

    creates the MethodCallInsideLockAnalyzer signature inside the class

--- src/ParallelHelper.Test/Analyzer/Smells/MethodCallInsideLockAnalyzer.cs
+++ src/ParallelHelper.Test/Analyzer/Smells/MethodCallInsideLockAnalyzer.cs
@@ -10,10 +10,20 @@ using System.Threading.Tasks;
 namespace ParallelHelper.Test.Analyzer.Smells {
   [DiagnosticAnalyzer(LanguageNames.CSharp)]
   public class MethodCallInsideLockAnalyzer : DiagnosticAnalyzer {
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
+    public const string DiagnosticId = "PH_BT004";
+    private const string Category = "Locking";
 
+    private static readonly LocalizableString Title = "Method call in a lock";
+    private static readonly LocalizableString MessageFormat = "A variable has been assigned inside the lock";
+    private static readonly LocalizableString Description = "";
+
+    private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+     DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error,
+     isEnabledByDefault: true, description: Description, helpLinkUri: ""//gets the .md file from parallell helper github HelpLinkFactory.CreateUri(DiagnosticId)
+   );
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
     public override void Initialize(AnalysisContext context) {
-      throw new NotImplementedException();
+      
     }
   }
 }
--- src/ParallelHelper.Test/Analyzer/Smells/MethodCallInsideLockAnalyzerTest.cs
+++ src/ParallelHelper.Test/Analyzer/Smells/MethodCallInsideLockAnalyzerTest.cs
@@ -8,5 +8,24 @@ using System.Threading.Tasks;
 namespace ParallelHelper.Test.Analyzer.Smells {
   [TestClass]
   public class MethodCallInsideLockAnalyzerTest:AnalyzerTestBase<MethodCallInsideLockAnalyzer> {
+    [TestMethod]
+    public void PrivateFieldLockedInClass() {
+      var source = @"public class Class
+      {
+            private readonly object lockObject = new object();
+            
+            private int MyNumber;
+            public string MyText;
+
+            public void DoWork()
+            {
+                lock (lockObject)
+                {
+                    MyNumber+=1;
+                }
+        }
+      }";
+      VerifyDiagnostic(source);
+    }
   }
 }

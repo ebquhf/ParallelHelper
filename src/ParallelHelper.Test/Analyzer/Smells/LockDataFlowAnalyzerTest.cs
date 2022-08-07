a261e6eeb2823a8d1c39a5a1dd9c5d5ecbe33c72
ebquhf <barna211@vipmail.hu>
2022-08-07 21:13:51 +0200

    creates the LockDataFlowAnalyzer skeleton

---
+++ src/ParallelHelper.Test/Analyzer/Smells/LockDataFlowAnalyzerTest.cs
@@ -0,0 +1,29 @@
+﻿using ParallelHelper.Analyzer.Smells;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelHelper.Test.Analyzer.Smells {
+  public class LockDataFlowAnalyzerTest : AnalyzerTestBase<LockDataFlowAnalyzer> {
+    public void PrivateFieldLockedInClass() {
+      var source = @"public class Class
+      {
+            private readonly object lockObject = new object();
+            
+            private int MyNumber;
+            public string MyText;
+
+            public void DoWork()
+            {
+                lock (lockObject)
+                {
+                    MyNumber+=1;
+                }
+        }
+      }";
+      VerifyDiagnostic(source);
+    }
+  }
+}
---
+++ src/ParallelHelper/Analyzer/Smells/LockDataFlowAnalyzer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+
+namespace ParallelHelper.Analyzer.Smells {
+  [DiagnosticAnalyzer(LanguageNames.CSharp)]
+  public class LockDataFlowAnalyzer : DiagnosticAnalyzer {
+    public const string DiagnosticId = "PH_BT005";
+    private const string Category = "Locking";
+
+    private static readonly LocalizableString Title = "Dataflow inside lock";
+    private static readonly LocalizableString MessageFormat = "A variable is used in a safe way regarding concurrency inside the lock";
+    private static readonly LocalizableString Description = "";
+
+    private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+     DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error,
+     isEnabledByDefault: true, description: Description, helpLinkUri: ""//gets the .md file from parallell helper github HelpLinkFactory.CreateUri(DiagnosticId)
+   );
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+    public override void Initialize(AnalysisContext context) {
+      context.RegisterSyntaxNodeAction(AnalyzeLockStatement, SyntaxKind.LockStatement);
+    }
+
+    private void AnalyzeLockStatement(SyntaxNodeAnalysisContext context) {
+      var model = context.SemanticModel;
+      var lockstatement = context.Node as LockStatementSyntax;
+      if(model != null && lockstatement != null) {
+
+        DataFlowAnalysis result = model.AnalyzeDataFlow(lockstatement);
+
+        Console.WriteLine(result.Succeeded);
+      }
+
+
+    }
+
+  }
+}

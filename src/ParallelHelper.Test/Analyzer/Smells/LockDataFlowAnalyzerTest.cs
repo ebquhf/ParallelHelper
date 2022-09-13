d40bd42606fd020269f3c094d511f233782085ec
ebquhf <barna211@vipmail.hu>
2022-09-13 21:37:25 +0200

    creates new test cases for the LockDataFlowAnalyzer

--- src/ParallelHelper.Test/Analyzer/Smells/LockDataFlowAnalyzerTest.cs
+++ src/ParallelHelper.Test/Analyzer/Smells/LockDataFlowAnalyzerTest.cs
@@ -1,4 +1,5 @@
-﻿using ParallelHelper.Analyzer.Smells;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelHelper.Analyzer.Smells;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,20 +7,34 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ParallelHelper.Test.Analyzer.Smells {
+  [TestClass]
   public class LockDataFlowAnalyzerTest : AnalyzerTestBase<LockDataFlowAnalyzer> {
+    [TestMethod]
     public void PrivateFieldLockedInClass() {
-      var source = @"public class Class
+      var source = @"
+      public class Class
       {
             private readonly object lockObject = new object();
             
             private int MyNumber;
             public string MyText;
-
+[ThreadStatic] volatile ThisEventHandler _localEvent = null; //need volatile to ensure we get the latest
+public virtual void OnThisEvent (EventArgs args)
+{
+   _localEvent=ThisEvent; //assign the event to a local variable – notice is volatile class field
+   if (_localEvent!=null)
+   {
+     _localEvent(this,args);
+   }
+}
             public void DoWork()
             {
+               
                 lock (lockObject)
                 {
-                    MyNumber+=1;
+                
+                    MyNumber=1;
+                    OnThisEvent(MyNumber);
                 }
         }
       }";

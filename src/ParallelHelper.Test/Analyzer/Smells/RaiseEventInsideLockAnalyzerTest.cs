673cacec6d697fb743d03db8395361753cc6e2f3
ebquhf <barna211@vipmail.hu>
2022-09-21 08:29:50 +0200

    creates test class for PH_S900

---
+++ src/ParallelHelper.Test/Analyzer/Smells/RaiseEventInsideLockAnalyzerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelHelper.Analyzer.Smells;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelHelper.Test.Analyzer.Smells {
+  [TestClass]
+  public class RaiseEventInsideLockAnalyzerTest : AnalyzerTestBase<RaiseEventInsideLockAnalyzer> {
+    [TestMethod]
+    public void RaiseEventNotInLock() {
+      const string source = @"public event MyEventHandler MyEvent;
+
+protected virtual OnMyEvent(MyEventArgs args)
+{
+    if (this.MyEvent != null)
+    {
+        this.MyEvent(this, args);
+    }
+}";
+      VerifyDiagnostic(source);
+
+    }
+  }
+}

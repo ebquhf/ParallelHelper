00b860e0dd6ab62f0d4d496d620a722293f17cec
andrzejolszak <andrzejolszak@gmail.com>
2022-09-22 22:46:58 +0200

    Added more tests for the assignement analyzer.

--- src/ParallelHelper.Test/Analyzer/Smells/AssignmentInsideLockAnalyzerTest.cs
+++ src/ParallelHelper.Test/Analyzer/Smells/AssignmentInsideLockAnalyzerTest.cs
@@ -28,9 +28,11 @@ namespace ParallelHelper.Test.Analyzer.Smells {
         }
       }";
       VerifyDiagnostic(source);
+      VerifyDiagnostic2(source);
     }
+
     [TestMethod]
-    public void PublicFieldLockedInClass() {
+    public void PrivateFieldPartiallyLockedInClass() {
       var source = @"public class Class
       {
             private readonly object lockObject = new object();
@@ -42,11 +44,108 @@ namespace ParallelHelper.Test.Analyzer.Smells {
             {
                 lock (lockObject)
                 {
-                    MyText=""text1"";
+                    MyNumber+=1;
+                }
+            }
+
+            public void DoDirtyWork()
+            {
+                MyNumber+=1; //ERR: non-synchronized access from a public method
+            }
+      }";
+      VerifyDiagnostic2(source);
+    }
+
+    [TestMethod]
+    public void PublicFieldMethodCallChain() {
+      var source = @"public class Class
+      {
+            private readonly object lockObject = new object();
+            
+            public int MyNumber;
+            public string MyText;
+
+            public void DoWork()
+            {
+                lock (lockObject)
+                {
+                    this.DoDirtyWork(); //ERR: control flow - non-synchronized access via called method
+                }
+            }
+
+            private void DoDirtyWork()
+            {
+                MyNumber+=1;
+            }
+      }";
+      VerifyDiagnostic2(source);
+    }
+
+    [TestMethod]
+    public void PrivateFieldLockedInsufficiently() {
+      var source = @"public class Class
+      {
+            private readonly object lockObject = new object();
+            
+            private int MyNumber;
+            public string MyText;
+
+            public void IncrementUntilMax2()
+            {
+                if (MyNumber < 2) //ERR: this would suggest that MyNumber should never become >2, but it can if multiple thread execute this simultaneously
+                {
+                  lock (lockObject)
+                  {
+                      MyNumber+=1;
+                  }
+                }
+            }
+      }";
+      VerifyDiagnostic2(source);
+    }
+
+    [TestMethod]
+    public void LeakyReference() {
+      var source = @"public class Class
+      {
+            private readonly object lockObject = new object();
+            
+            private int[] MyNumbers = new int[1];
+            public string MyText;
+
+            public void IncrementUntilMax2()
+            {
+                int[] hack = null;
+                lock (lockObject)
+                {
+                    hack = MyNumbers;
+                }
+
+                hack[0] = 42; //ERR: data flow - we are modifying MyNumbers object outside of the lock!
+            }
+      }";
+      VerifyDiagnostic2(source);
+    }
+
+    [TestMethod]
+    public void PublicFieldLockedInClass() {
+      var source = @"public class Class
+      {
+            private readonly object lockObject = new object();
+            
+            private int MyNumber;
+            public string MyText;
+
+            public void DoWork()
+            {
+                lock (lockObject) //ERR: public field - maybe the error should be on the field declaration line instead?
+                {
+                    MyText=""text1"" + MyText;
                 }
         }
       }";
       VerifyDiagnostic(source, new DiagnosticResultLocation(9, 17));
+      VerifyDiagnostic2(source);
     }
 
     [TestMethod]
@@ -62,12 +161,34 @@ namespace ParallelHelper.Test.Analyzer.Smells {
     }
 
     public void DoWork() {
-      lock(lockObject) {
+      lock(lockObject) { //ERR: public property
         MyProperty += 1;
       }
     }
   }";
       VerifyDiagnostic(source, new DiagnosticResultLocation(11, 7));
+      VerifyDiagnostic2(source);
+    }
+
+    [TestMethod]
+    public void PublicPropertyPublicBackingField() {
+      var source = @"public class Class {
+    
+    private readonly object lockObject = new object();
+
+    private int testProperty;
+    public int MyProperty {
+      get { return testProperty; }
+      set { testProperty = value; }
+    }
+
+    public void DoWork() {
+      lock(lockObject) { //ERR: modifies backing field
+        testProperty += 1;
+      }
+    }
+  }";
+      VerifyDiagnostic2(source);
     }
 
     [TestMethod]
@@ -89,6 +210,23 @@ namespace ParallelHelper.Test.Analyzer.Smells {
     }
   }";
       VerifyDiagnostic(source);
+      VerifyDiagnostic2(source);
+    }
+
+    private void VerifyDiagnostic2(string source)
+    {
+      string[] lines = source.Split("\r\n");
+      List<DiagnosticResultLocation> diagnostics = new List<DiagnosticResultLocation>();
+      for(int i = 0; i < lines.Length; i++)
+      {
+        if(lines[i].Contains("//ERR"))
+        {
+          diagnostics.Add(new DiagnosticResultLocation(i, lines[i].IndexOf(lines[i].Trim()) + 1));
+          break;
+        }
+      }
+
+      VerifyDiagnostic(source, diagnostics.ToArray());
     }
   }
 }

b65d5ad10dee89f60ceec6c22903f743dac04fdc
ebquhf <barna211@vipmail.hu>
2022-11-06 20:43:54 +0100

    ceates EventLockAnalyzer skeleton

---
+++ src/ParallelHelper/Analyzer/Smells/EventLockAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using ParallelHelper.Util;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace ParallelHelper.Analyzer.Smells {
+  [DiagnosticAnalyzer(LanguageNames.CSharp)]
+  public class EventLockAnalyzer : DiagnosticAnalyzer {
+    public const string DiagnosticId = "BT_007";
+
+    private const string Category = "Concurrency";
+
+    private static readonly LocalizableString Title = "Raise event in constructor";
+    private static readonly LocalizableString MessageFormat = "Rasing an event inside a lock is discouraged.";
+    private static readonly LocalizableString Description = "";
+
+    private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+      DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning,
+      isEnabledByDefault: true, description: Description, helpLinkUri: HelpLinkFactory.CreateUri(DiagnosticId)
+    );
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+    public override void Initialize(AnalysisContext context) {
+      throw new NotImplementedException();
+    }
+  }
+}

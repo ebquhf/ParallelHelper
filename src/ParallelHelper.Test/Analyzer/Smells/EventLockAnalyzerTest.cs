9858f34944d999ea5ecdde04b2c391cfeeb16c98
ebquhf <barna211@vipmail.hu>
2022-11-07 21:35:02 +0100

    fixes the error in test

---
+++ src/ParallelHelper.Test/Analyzer/Smells/EventLockAnalyzerTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelHelper.Analyzer.Smells;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelHelper.Test.Analyzer.Smells {
+  [TestClass]
+  public class EventLockAnalyzerTest : AnalyzerTestBase<EventLockAnalyzer> {
+    [TestMethod]
+    public void RaiseEventNotInLock() {
+      const string source = @"public event MyEventHandler MyEvent;
+
+protected virtual OnMyEvent(MyEventArgs args)
+{
+    if (this.MyEvent != null)
+    {
+        this.MyEvent(this, args);
+    }
+}";
+      VerifyDiagnostic(source);
+
+    }
+    [TestMethod]
+    public void PrivateFieldLockedInClass() {
+      var source = @"
+      public class EventClass
+    {
+        private readonly object lockObject = new object();
+        public delegate void ThisEvent(int number);
+        private int MyNumber;
+        public string MyText;
+
+        private event ThisEvent MyEvent;
+
+        public virtual void OnThisEvent(int number)
+        {
+            MyEvent?.Invoke(number);
+        }
+        public void DoWork()
+        {
+
+            lock (lockObject)
+            {
+                MyEvent += OnThisEvent;
+                MyNumber = 1;
+                OnThisEvent(MyNumber);
+            }
+        }
+
+
+    }";
+      VerifyDiagnostic(source);
+    }
+  }
+}
--- src/ParallelHelper/Analyzer/Smells/EventLockAnalyzer.cs
+++ src/ParallelHelper/Analyzer/Smells/EventLockAnalyzer.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using ParallelHelper.Util;
 using System;
@@ -25,7 +27,33 @@ namespace ParallelHelper.Analyzer.Smells {
     public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
     public override void Initialize(AnalysisContext context) {
-      throw new NotImplementedException();
+      context.EnableConcurrentExecution();
+      context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+      context.RegisterSyntaxNodeAction(AnalyzeExpressionStatement, SyntaxKind.ConstructorDeclaration);
+    }
+    private static void AnalyzeExpressionStatement(SyntaxNodeAnalysisContext context) {
+      new Analyzer(context).Analyze();
+    }
+    private class Analyzer : MonitorAwareAnalyzerWithSyntaxWalkerBase<ConstructorDeclarationSyntax> {
+      public Analyzer(SyntaxNodeAnalysisContext context) : base(new SyntaxNodeAnalysisContextWrapper(context)) {
+        var node = context.Node as SimpleNameSyntax;
+
+        // we're only interested in delegates
+        var type = SemanticModel.GetTypeInfo(node, context.CancellationToken).ConvertedType;
+
+        if(type == null || type.TypeKind != TypeKind.Delegate) {
+          return;
+        }
+
+        // we're only interested in methods from the current assembly
+        var symbol = context.SemanticModel.GetSymbolInfo(node, context.CancellationToken).Symbol;
+
+        if(symbol == null ||
+            symbol.Kind != SymbolKind.Method ||
+            !symbol.ContainingAssembly.Equals(context.SemanticModel.Compilation.Assembly)) {
+          return;
+        }
+      }
     }
   }
 }
